130933232572770075f3ff362dc4cdadfd73a8e3
ryuto <ryuto2007.11.27@gmail.com>
2024-05-21 19:31:34 +0900

    a

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def submit():
         response = openai.Image.create(
             prompt=prompt,
             n=1,
-            size="1024x1024"
+            size="256x256"
         )
         image_url = response['data'][0]['url']
         app.logger.info("Generated image URL: %s", image_url)
